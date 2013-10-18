806a34048ff84d3aaef57a0e5a528eae685510a0
richardfergie <richard.fergie+github@gmail.com>
2013-10-18 14:10:45 +0100

    Switch to using conduit

--- bitcoin-blockchain.hs
+++ bitcoin-blockchain.hs
@@ -1,39 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as BS
-import Data.Binary hiding (encode, decode)
+import Data.Binary hiding (encode)
 import Data.Binary.Get
 import Control.Monad
 import Text.Bytedump
 import System.Directory
-import Data.List
+import Data.List (sort)
 import Numeric
 import qualified Data.Text as T
-import Data.ByteString.Base16 (encode, decode)
+import Data.ByteString.Base16 (encode)
 
+import Data.Conduit.Serialization.Binary
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+import Data.Conduit
+import System.IO
 
-main = do
-  blocks <- parseFiles
-  print $ last $ concatMap getResponseScripts blocks
+--reads from stdin
+--parses blocks
+--prints to stdout
+main = source $$ conduit =$ CL.mapM_ print
 
-getResponseScripts block = scripts
-  where trans = transactions block
-        inp = concatMap transactioninputs trans
-        scripts = map responsescript inp
-        
+source :: Source IO BS.ByteString
+source = CB.sourceHandle stdin
+
+sink :: Sink Block IO ()
+sink = CL.mapM_ print
+
+conduit :: Conduit BS.ByteString IO Block
+conduit = conduitGet getBlock
+
+parseFiles :: IO [Block]
 parseFiles = do
   home <- getHomeDirectory
   c <- getDirectoryContents $ home++"/.bitcoin/blocks/"
   let blockfiles = init $ sort $ filter (\x -> take 3 x == "blk") c
   blocks <- mapM parseFile blockfiles
   return $ concat blocks
-      
+
+parseFile :: String -> IO [Block]
 parseFile f = do
   home <- getHomeDirectory
   raw <- BL.readFile $ home++"/.bitcoin/blocks/"++f
   let blocks = runGet getBlocks raw
-  return blocks
-      
+  return $ blocks
+
 data Block = Block {
                magic :: !T.Text,
                blocklength :: !Int,
@@ -73,16 +85,21 @@ data Output = Output {
 getBlock :: Get Block
 getBlock = do
   mag <- getWord32be
-  blength <- getWord32le
-  bformatv <- getWord32le
-  pblockh <- getByteString 32
-  merkle <- getByteString 32
-  t <- getWord32le
-  b <- getWord32be
-  n <- getWord32be
-  tnumber <- getVarLengthInt
-  trans <- replicateM (fromIntegral tnumber) getTransaction
-  return $ Block (textHex mag)
+  --sometimes (only in blk00066.dat so far) there are long periods of zeros
+  --I don't know what these mean, but it messes up the parsing.
+  --So if a block doesn't start with f9beb4d9 then skip forward until it does
+  case (textHex mag) of
+    "f9beb4d9" -> do
+      blength <- getWord32le
+      bformatv <- getWord32le
+      pblockh <- getByteString 32
+      merkle <- getByteString 32
+      t <- getWord32le
+      b <- getWord32be
+      n <- getWord32be
+      tnumber <- getVarLengthInt
+      trans <- replicateM (fromIntegral tnumber) getTransaction
+      return $! Block (textHex mag)
                  (fromIntegral blength)
                  (fromIntegral bformatv)
                  (encode pblockh)
@@ -92,8 +109,10 @@ getBlock = do
                  (textHex n)
                  (fromIntegral tnumber)
                  trans
+    otherwise -> getBlock
 
-textHex x = T.pack $ showHex x ""
+textHex :: Word32 -> T.Text
+textHex x = T.pack $! showHex x ""
 
 getTransaction :: Get Transaction
 getTransaction = do
@@ -103,7 +122,7 @@ getTransaction = do
   outputc <- getVarLengthInt
   outputs <- replicateM (fromIntegral outputc) getOutput
   locktime <- getWord32le
-  return $ Transaction (fromIntegral ver)
+  return $! Transaction (fromIntegral ver)
                        (fromIntegral inputc)
                        inputs
                        (fromIntegral outputc)
@@ -118,7 +137,7 @@ getInput = do
   rlength <- getVarLengthInt
   script <- getByteString (fromIntegral rlength)
   seqnum <- getWord32le
-  return $ Input (encode hash)
+  return $! Input (encode hash)
                  (textHex index)
                  (fromIntegral rlength)
                  (encode script)
@@ -129,7 +148,7 @@ getOutput = do
   val <- getWord64le
   slength <- getVarLengthInt
   script <- getByteString (fromIntegral slength)
-  return $ Output (fromIntegral val)
+  return $! Output (fromIntegral val)
                   (fromIntegral slength)
                   (encode script)
 
@@ -149,11 +168,11 @@ getVarLengthInt = do
   case hex of
     "fd" -> do
       val <- getWord16le
-      return $ fromIntegral val
+      return $! fromIntegral val
     "fe" -> do
       val <- getWord32le
-      return $ fromIntegral val
+      return $! fromIntegral val
     "ff" -> do
       val <- getWord64le
-      return $ fromIntegral val
-    otherwise -> return $ fromIntegral w  
+      return $! fromIntegral val
+    otherwise -> return $! fromIntegral w  
